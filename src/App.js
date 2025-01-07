37333f6eb012aeabfedbb901307a24ff237f60b7
ILYA <94749164+sm1k0@users.noreply.github.com>
2025-01-07 14:05:49 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import HeartAnimation from './HeartAnimation';
-import { Analytics } from "@vercel/analytics/react"
+
 function App() {
   return (
     <div style={{ margin: 0, padding: 0, overflow: 'hidden' }}>
